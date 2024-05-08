43028cf1cc70eea18ccd153e3a72f9279f24b0d8
Manav Gondalia <gondaliamanav@gmail.com>
2024-05-08 11:24:05 +0530

    Add extraction from .env

--- mental-health-server/db.js
+++ mental-health-server/db.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql");
 const dotenv = require("dotenv");
 
+dotenv.config();
+
 const db = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "1234",
-	database: "mental_health",
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
 });
 
 // export the db object
