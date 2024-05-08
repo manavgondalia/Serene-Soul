26c06ad83a6db1b8e3ec698493279ca8fa04b12c
Manav Gondalia <gondaliamanav@gmail.com>
2024-05-08 11:48:47 +0530

    Update servers for .env

--- mental-health-server/server.js
+++ mental-health-server/server.js
@@ -10,11 +10,12 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
+app.use(cors());
+
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
